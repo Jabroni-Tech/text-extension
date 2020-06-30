303ea5a7105c6d9ab657a6c2ba0a5431906fa8ab
Jason McCutchan <jason.mccutchan@gmail.com>
2020-06-29 20:46:15 -0400

    added onClick

---
+++ chrome/js/onClick.js
@@ -0,0 +1,8 @@
+
+window.addEventListener("click", function(event) {
+  console.log('were in the clikc WINDOW', event)
+});
+
+document.addEventListener("click", function(event) {
+    console.log('were in the clikc DOCUMENT', event)
+});
